333adc32999aa3ea16cba3f34aaa93d087c6dd7a
Aditya Bhargav <adityabhargav9876@gmail.com>
2022-01-02 22:44:34 +0530

    Add prediction of multiple faces

--- lbph_face_rec/main.py
+++ lbph_face_rec/main.py
@@ -6,25 +6,29 @@ import numpy as np
 ocvfiles = 'opencv-files'
 #using lbp as its faster on embedded devices
 cclassifier = 'lbpcascade_frontalface.xml'
-subjects = ["","Amber Heard","Bill Gates","Jason Momoa","Paul Rudd","Scarlett Johansson"]
+subjects = ["", "Amber Heard", "Bill Gates", "Jason Momoa", "Paul Rudd", 
+        "Scarlett Johansson"]
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 def detect_face(img):
-    #convert the test image to gray image as opencv face detector expects gray images
+    #convert grayscale as opencv face detector expects gray images
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #load OpenCV face detector
     face_cascade = cv2.CascadeClassifier(os.path.join(ocvfiles, cclassifier))
-    #let's detect multiscale (some images may be closer to camera than others) images
+    #some images may be closer to camera than others
     #result is a list of faces
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);   
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2,
+            minNeighbors=5);   
     #if no faces are detected then return original img
     if (len(faces) == 0):
         return None, None   
-    #under the assumption that there will be only one face,
-    #extract the face area
-    (x, y, w, h) = faces[0] 
+    pixels = []
+    for face in faces:
+        #extract the face area
+        (x, y, w, h) = face
+        pixels.append(gray[y:y+w, x:x+h])
     #return only the face part of the image
-    return gray[y:y+w, x:x+h], faces[0]
+    return pixels, faces
 
 def prepare_training_data(data_folder_path):
     dirs = os.listdir(data_folder_path)
@@ -41,12 +45,12 @@ def prepare_training_data(data_folder_path):
                 continue;
             image_path = subject_dir_path + "/" + image_name
             image = cv2.imread(image_path)
-            image2=cv2.resize(image,(400,500))
+            image2=cv2.resize(image, (400, 500))
             cv2.imshow("Training on image...", image2)
             cv2.waitKey(100)
-            face, rect = detect_face(image2)
-            if face is not None:
-                faces.append(cv2.resize(face,(100,200)))
+            pixels = detect_face(image2)[0]
+            if pixels is not None:
+                faces.append(cv2.resize(pixels[0], (100, 200)))
                 labels.append(label)         
     cv2.destroyAllWindows()
     cv2.waitKey(1)
@@ -61,22 +65,22 @@ def draw_text(img, text, x, y):
     cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
 def predict(test_img):
-    #make a copy of the image as we don't want to chang original image
+    #make a copy of the image as we don't want to change original image
     img = test_img.copy()
     #detect face from the image
-    image2=cv2.resize(img,(400,500))
-    face, rect = detect_face(image2)
+    image2=cv2.resize(img, (400, 500))
+    faces, rects = detect_face(image2)
     #predict the image using our face recognizer 
-    if face is not None:
-        label, confidence = face_recognizer.predict(cv2.resize(face,(100,200)))
-        #get name of respective label returned by face recognizer
-        label_text = subjects[label]
-        
-        #draw a rectangle around face detected
-        draw_rectangle(image2, rect)
-        #draw name of predicted person
-        draw_text(image2, label_text, rect[0], rect[1]-5)
-        
+    if faces is not None:
+        for i in range(len(faces)):
+            label, confidence = face_recognizer.predict(cv2.resize(faces[i],
+                (100, 200)))
+            #get name of respective label returned by face recognizer
+            label_text = subjects[label] + "(" + str(confidence) + "%)"
+            #draw a rectangle around face detected
+            draw_rectangle(image2, rects[i])
+            #draw name of predicted person
+            draw_text(image2, label_text, rects[i][0], rects[i][1]-5)
         return image2
     else:
         draw_text(image2,"No face detected",6,25)
@@ -88,7 +92,7 @@ def predict_all():
         imagepath = "test-data/" + imagename
         test_img=cv2.imread(imagepath)
         predicted_img=predict(test_img)
-        cv2.imshow(imagename,cv2.resize(predicted_img,(400,500)))
+        cv2.imshow(imagename, cv2.resize(predicted_img, (400, 500)))
         cv2.waitKey(3000)
         cv2.destroyAllWindows()
         
@@ -99,7 +103,7 @@ def videoCap():
         check, frame = webcam.read()
         print(check)
         print(frame)
-        frame=cv2.flip(frame,1)
+        frame=cv2.flip(frame, 1)
         cv2.imshow("Capturing", frame)
         key = cv2.waitKey(1)
         if key == ord('s'): 
@@ -107,14 +111,14 @@ def videoCap():
             #fname='webcamtest'+str(random.randint(100,200))+'.jpg'
             #cv2.imwrite(filename=fname, img=frame)
             webcam.release()
-            cv2.imshow('Captured image',frame)
+            cv2.imshow('Captured image', frame)
             cv2.waitKey(1650)
             cv2.destroyAllWindows()
             #os.chdir(r'C:\Users\admin\Desktop\facerec')
             #print("Image saved!")
             #image=cv2.imread("test-data/"+fname)
             img=predict(frame)
-            cv2.imshow('Captured image prediction',img)
+            cv2.imshow('Captured image prediction', img)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
             break
@@ -126,16 +130,19 @@ def videoCap():
             cv2.destroyAllWindows()
             break
 
-def main():
+def prepare_data():
     print("Preparing data...")
     faces, labels = prepare_training_data("training-data")
     print("Data prepared")
     print("Total faces: ", len(faces))
     print("Total labels: ", len(labels))
     face_recognizer.train(faces, np.array(labels))
-    #face_recognizer.write('eigen.cv2')
-    #print("Face recognizer written to disk")
-    #face_recognizer.read('eigen.cv2')
+    face_recognizer.write('eigen.cv2')
+    print("Face recognizer written to disk")
+
+def main():
+    #prepare_data()
+    face_recognizer.read('eigen.cv2')
     predict_all()
 
 if __name__ == "__main__":

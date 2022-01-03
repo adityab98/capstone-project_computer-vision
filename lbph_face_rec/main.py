349077a9dbc971facfd35736178128ffdc7aba8f
Aditya Bhargav <adityabhargav9876@gmail.com>
2022-01-03 12:10:24 +0530

    Live video capture detects faces

--- lbph_face_rec/main.py
+++ lbph_face_rec/main.py
@@ -142,8 +142,21 @@ def prepare_data():
 
 def main():
     #prepare_data()
-    face_recognizer.read('eigen.cv2')
-    predict_all()
+    #face_recognizer.read('eigen.cv2')
+    #predict_all()
+    face_cascade = cv2.CascadeClassifier(os.path.join(ocvfiles, cclassifier))
+    video_capture = cv2.VideoCapture(0)
+    while True:
+        rect, frames = video_capture.read()
+        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1,
+                minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #Display the resulting frame
+        cv2.imshow('Video', frames)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 if __name__ == "__main__":
     main()

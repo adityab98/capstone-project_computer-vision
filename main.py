24ae9218968296a6803a5c713336dee430422be9
Aditya Bhargav <adityabhargav9876@gmail.com>
2022-02-06 12:22:33 +0530

    Modify  main, notifier and audio main.py: remove password as argument. Set default password as None notifier.py: if password is none, will return from notify() function audio.py: Recorder class now records in single channel for librosa

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from video import *
 from notifier import *
 from audio import *
 
-def main(password):
+def main():
+    if (len(sys.argv) != 2):
+        print("No valid password specified. Setting password as None.")
+        password = None
+    else:
+        password = sys.argv[1]
     sender_address = 'adityabhargav9876@gmail.com'
     physical_address = 'XXX YYY ZZZ, India'
     ocv_files = 'opencv_files'
@@ -14,19 +19,14 @@ def main(password):
             "Scarlett Johansson"]
     img_len = 500
     img_width = 400
-    """
     face_rec = Face_Recognizers(os.path.join(ocv_files, cascade_classifier),
             img_len, img_width, subjects, os.path.join("data_files",
                 "face_training"), os.path.join("data_files", "face_test"))
     notifier = Notifier(sender_address, physical_address, sender_address,
             password)
-    voice_rec = Voice_Recognizer(os.path.join("data_files", "voice_training"))
-    """
     recorder = Recorder()
-    recorder.record()
+    voice_rec = Voice_Recognizer(os.path.join("data_files", "voice_training"))
     return 0
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 2):
-        print("Please enter your gmail password as an argument to the program.")
-    main(sys.argv[1])
+    main()

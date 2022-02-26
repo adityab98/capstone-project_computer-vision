72b09c102420363acbf5bd890aca21c095adbbd4
Aditya Bhargav <adityabhargav9876@gmail.com>
2022-02-26 17:01:49 +0530

    Update audio.py for research

--- audio.py
+++ audio.py
@@ -1,12 +1,15 @@
-import soundfile # to read audio file
+import soundfile
 import numpy as np
-import librosa # to extract speech features
+import librosa
 import glob
 import os
 import pickle
-from sklearn.neural_network import MLPClassifier # multi-layer perceptron model
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
 import sounddevice as sd
 from scipy.io.wavfile import write
+from sklearn.metrics import accuracy_score
+from itertools import permutations, combinations
 
 class Voice_Recognizer:
 
@@ -86,8 +89,8 @@ class Voice_Recognizer:
                     mel = True, contrast = True, tonnetz = True)
             X.append(features)
             y.append(emotion)
-        return np.array(X), y
-    
+        return np.array(X), y   
+        
     def train(self):
         X_train, y_train = self.load_data()
         print("[+] Number of training samples:", X_train.shape[0])
@@ -105,12 +108,86 @@ class Voice_Recognizer:
         model.fit(X_train, y_train)
         pickle.dump(model, open("mlp_classifier.model", "wb"))
         return model
-
+    
     def predict(self, audio_src_file):
         features = self.extract_feature(audio_src_file, mfcc = True, chroma =
                 True, mel = True, contrast = True, tonnetz = True)
         return self.model.predict(np.array([features]))[0]
-
+        
+    def research_extract_feature(self, file_name):
+        print("extracting features for " + file_name)
+        with soundfile.SoundFile(file_name) as sound_file:
+            X = sound_file.read(dtype = "float32")
+            sample_rate = sound_file.samplerate
+            stft = np.abs(librosa.stft(X))
+            mfccs = np.mean(librosa.feature.mfcc(y = X, sr = sample_rate, n_mfcc = 40).T, axis = 0)
+            chroma = np.mean(librosa.feature.chroma_stft(S = stft, sr = sample_rate).T, axis = 0)
+            mel = np.mean(librosa.feature.melspectrogram(y = X, sr = sample_rate).T, axis = 0)
+            contrast = np.mean(librosa.feature.spectral_contrast(S = stft, sr = sample_rate).T, axis = 0)
+            tonnetz = np.mean(librosa.feature.tonnetz(y = librosa.effects.harmonic(X), sr = sample_rate).T, axis = 0)
+        return mfccs, chroma, mel, contrast, tonnetz
+        
+    def research_load_data(self):
+        mfccs_arr = []
+        chromas_arr = []
+        mels_arr = []
+        contrasts_arr = []
+        tonnetzs_arr = []
+        y = []
+        for file in glob.glob(os.path.join(self.training_data, "Actor_*", "*.wav")):
+            basename = os.path.basename(file)
+            emotion = self.int2emotion[basename.split("-")[2]]
+            mfcc, chroma, mel, contrast, tonnetz = self.research_extract_feature(file)
+            mfccs_arr.append(mfcc)
+            chromas_arr.append(chroma)
+            mels_arr.append(mel)
+            contrasts_arr.append(contrast)
+            tonnetzs_arr.append(tonnetz)
+            y.append(emotion)
+        pickle.dump(mfccs_arr, open("mfcc.list", "wb"))    
+        pickle.dump(chromas_arr, open("chroma.list", "wb"))    
+        pickle.dump(mels_arr, open("mel.list", "wb"))    
+        pickle.dump(contrasts_arr, open("contrast.list", "wb"))    
+        pickle.dump(tonnetzs_arr, open("tonnetz.list", "wb"))    
+        pickle.dump(y, open("y.list", "wb"))
+    
+    def research_model(self):
+        mfccs_arr = pickle.load(open("mfcc.list", "rb"))
+        chromas_arr = pickle.load(open("chroma.list", "rb"))
+        mels_arr = pickle.load(open("mel.list", "rb"))
+        contrasts_arr = pickle.load(open("contrast.list", "rb"))
+        tonnetzs_arr = pickle.load(open("tonnetz.list", "rb"))
+        y = pickle.load(open("y.list", "rb"))
+        X = []
+        for i in range(len(mfccs_arr)):
+            tmp = np.array([])
+            tmp = np.hstack((tmp, mfccs_arr[i]))
+            tmp = np.hstack((tmp, chromas_arr[i]))
+            tmp = np.hstack((tmp, mels_arr[i]))
+            tmp = np.hstack((tmp, contrasts_arr[i]))
+            tmp = np.hstack((tmp, tonnetzs_arr[i]))
+            X.append(tmp)
+            del tmp
+        X_train, X_test, y_train, y_test = train_test_split(np.array(X), y, test_size = 0.2, random_state = 7)
+        print("[+] Number of training samples:", X_train.shape[0])
+        print("[+] Number of testing samples:", X_test.shape[0])
+        print("[+] Number of features:", X_train.shape[1])
+        model_params = {
+            'alpha': 0.01,
+            'batch_size': 256,
+            'epsilon': 1e-08, 
+            'hidden_layer_sizes': (300,), 
+            'learning_rate': 'adaptive', 
+            'max_iter': 500, 
+        }
+        model = MLPClassifier(**model_params)
+        print("[*] Training the model...")
+        model.fit(X_train, y_train)
+        pickle.dump(model, open("mlp_classifier.model", "wb"))
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_true = y_test, y_pred = y_pred)
+        print("Accuracy: {:.2f}%".format(accuracy*100))
+        
 class Recorder:
 
     def __init__(self):
--- main.py
+++ main.py
@@ -1,40 +1,32 @@
-#!/usr/bin/env python3
 import sys
 import os
-from video import *
-from notifier import *
+# from video import *
 from audio import *
+import numpy as np
 
 def main():
-    if (len(sys.argv) != 2):
-        print("No valid password specified. Setting password as None.")
-        password = None
-    else:
-        password = sys.argv[1]
-    sender_address = 'adityabhargav9876@gmail.com'
-    physical_address = 'XXX YYY ZZZ, India'
     ocv_files = 'opencv_files'
     cascade_classifier = 'haarcascade_frontalface_alt.xml'
     subjects = ["", "Amber Heard", "Bill Gates", "Jason Momoa", "Paul Rudd", 
             "Scarlett Johansson"]
     img_len = 400
     img_width = 300
-    face_rec = Face_Recognizers(os.path.join(ocv_files, cascade_classifier),
-            img_len, img_width, subjects, os.path.join("data_files",
-                "face_training"), os.path.join("data_files", "face_test"))
-    notifier = Notifier(sender_address, physical_address, sender_address,
-            password)
+    # face_rec = Face_Recognizers(os.path.join(ocv_files, cascade_classifier),
+            # img_len, img_width, subjects, os.path.join("data_files",
+                # "face_training"), os.path.join("data_files", "face_test"))
     voice_rec = Voice_Recognizer(os.path.join("data_files", "voice_training"))
-    recorder = Recorder()
-    #face_rec.predict_all()
-    #face_rec.video_cap()
-    files = os.listdir("demo")
-    for file in files:
-        print(file, end = ' ')
-        print(voice_rec.predict(os.path.join("demo", file)))
-    #recorder.record()
-    #voice_rec.predict("live_recording.wav")
-    #notifier.notify(cv2.imread("data_files/face_test/test10.jpg"))
+    # recorder = Recorder()
+    # face_rec.predict_all()
+    # face_rec.video_cap()
+    # files = os.listdir("demo")
+    # for file in files:
+        # print(file, end = ' ')
+        # print(voice_rec.predict(os.path.join("demo", file)))
+    # recorder.record()
+    # voice_rec.predict("live_recording.wav")
+    voice_rec.research_model()
+    # voice_rec.research_load_data()
 
+    
 if __name__ == "__main__":
     main()

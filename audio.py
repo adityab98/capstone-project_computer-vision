a991b6490ddf491ec952f9746a828b17262e7116
Aditya Bhargav <adityabhargav9876@gmail.com>
2022-03-19 16:22:10 +0530

    Some progress

--- audio.py
+++ audio.py
@@ -6,13 +6,12 @@ import os
 import pickle
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
-import sounddevice as sd
-from scipy.io.wavfile import write
 from sklearn.metrics import accuracy_score
-from itertools import permutations, combinations
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 class Voice_Recognizer:
-
     def __init__(self, training_data):
         self.int2emotion = {
                 "01": "neutral", 
@@ -25,97 +24,32 @@ class Voice_Recognizer:
                 "08": "surprised"
         }
         self.training_data = training_data
-        # if (os.path.exists("mlp_classifier.model")):
-            # print("Voice_Recognizer: Pre-trained model found...")
-            # self.model = pickle.load(open("mlp_classifier.model", 'rb'))
-        # else:
-            # print("Voice_Recognizer: No pre-trained model found...")
-            # self.model = self.train()
-            
-    def extract_feature(self, file_name, **kwargs):
-        # Mel Frequency Cepstral Coefficient, represents the short-term power
-        # spectrum of a sound
-        mfcc = kwargs.get("mfcc")
-        # the 12 different pitch classes
-        chroma = kwargs.get("chroma")
-        # Mel Spectrogram Frequency
-        mel = kwargs.get("mel")
-        # Each frame of a spectogram is divided into bands. for each band,
-        # compute the contrast by comparing mean energy in top quartile to that
-        # of the bottom quartile. high contrast = clear signals. 
-        # low contrast = noise
-        contrast = kwargs.get("contrast")
-        # project chroma feature onto 6-dimensional basis
-        tonnetz = kwargs.get("tonnetz")
-        with soundfile.SoundFile(file_name) as sound_file:
-            X = sound_file.read(dtype = "float32")
-            sample_rate = sound_file.samplerate
-            if chroma or contrast:
-                # short-time fourier transform
-                # represents a signal in time-frequency domain by computing
-                # discrete fourier transforms over short overlapping windows
-                stft = np.abs(librosa.stft(X))
-            # contains all the features stacked horizontally
-            result = np.array([])
-            if mfcc:
-                mfccs = np.mean(librosa.feature.mfcc(y = X, sr = sample_rate,
-                    n_mfcc = 40).T, axis = 0)
-                result = np.hstack((result, mfccs))
-            if chroma:
-                chroma = np.mean(librosa.feature.chroma_stft(S = stft, sr =
-                    sample_rate).T, axis = 0)
-                result = np.hstack((result, chroma))
-            if mel:
-                mel = np.mean(librosa.feature.melspectrogram(y = X, sr =
-                    sample_rate).T, axis = 0)
-                result = np.hstack((result, mel))
-            if contrast:
-                contrast = np.mean(librosa.feature.spectral_contrast(S = stft,
-                    sr = sample_rate).T, axis = 0)
-                result = np.hstack((result, contrast))
-            if tonnetz:
-                tonnetz = np.mean(librosa.feature.tonnetz(y =
-                    librosa.effects.harmonic(X), sr = sample_rate).T, axis = 0)
-                result = np.hstack((result, tonnetz))
-        return result
-
-    def load_data(self):
-        X, y = [], []
-        for file in glob.glob(os.path.join(self.training_data, "Actor_*",
-            "*.wav")):
-            basename = os.path.basename(file)
-            emotion = self.int2emotion[basename.split("-")[2]]
-            features = self.extract_feature(file, mfcc = True, chroma = True,
-                    mel = True, contrast = True, tonnetz = True)
-            X.append(features)
-            y.append(emotion)
-        return np.array(X), y   
-        
-    def train(self):
-        X_train, y_train = self.load_data()
-        print("[+] Number of training samples:", X_train.shape[0])
-        print("[+] Number of features:", X_train.shape[1])
-        model_params = {
-            'alpha': 0.01,
-            'batch_size': 256,
-            'epsilon': 1e-08, 
-            'hidden_layer_sizes': (300,), 
-            'learning_rate': 'adaptive', 
-            'max_iter': 500, 
-        }
-        model = MLPClassifier(**model_params)
-        print("[*] Training the model...")
-        model.fit(X_train, y_train)
-        pickle.dump(model, open("mlp_classifier.model", "wb"))
-        return model
     
-    def predict(self, audio_src_file):
-        features = self.extract_feature(audio_src_file, mfcc = True, chroma =
-                True, mel = True, contrast = True, tonnetz = True)
-        return self.model.predict(np.array([features]))[0]
-        
-    def research_extract_feature(self, file_name):
-        print("extracting features for " + file_name)
+    def predict_ensemble(self, audio_src_file):
+        mfcc, chroma, mel, contrast, tonnetz = self.extract_feature(audio_src_file)
+        X = np.array([])
+        X = np.hstack((X, mfcc))
+        X = np.hstack((X, chroma))
+        X = np.hstack((X, mel))
+        X = np.hstack((X, contrast))
+        X = np.hstack((X, tonnetz))
+        X = [X]
+        X = np.array(X)
+        adam_model = pickle.load(open("mlp_adam.model", "rb"))
+        lbgfs_model = pickle.load(open("mlp_lbgfs.model", "rb"))
+        ensemble_model = pickle.load(open("ensembled.model", "rb"))
+        adam_preds = adam_model.predict_proba(X)
+        scaler = StandardScaler()
+        scaler.fit(X)
+        X = scaler.transform(X)
+        lbgfs_preds = lbgfs_model.predict_proba(X)
+        stacked_x = adam_preds
+        stacked_x = dstack((stacked_x, lbgfs_preds))
+        stacked_x = stacked_x.reshape((stacked_x.shape[0], stacked_x.shape[1]*stacked_x.shape[2]))
+        return ensemble_model.predict(stacked_x)[0]
+    
+    def extract_feature(self, file_name):
+        # print("extracting features for " + file_name)
         with soundfile.SoundFile(file_name) as sound_file:
             X = sound_file.read(dtype = "float32")
             sample_rate = sound_file.samplerate
@@ -126,8 +60,8 @@ class Voice_Recognizer:
             contrast = np.mean(librosa.feature.spectral_contrast(S = stft, sr = sample_rate).T, axis = 0)
             tonnetz = np.mean(librosa.feature.tonnetz(y = librosa.effects.harmonic(X), sr = sample_rate).T, axis = 0)
         return mfccs, chroma, mel, contrast, tonnetz
-        
-    def research_load_data(self):
+    
+    def save_training_data(self):
         mfccs_arr = []
         chromas_arr = []
         mels_arr = []
@@ -137,7 +71,7 @@ class Voice_Recognizer:
         for file in glob.glob(os.path.join(self.training_data, "Actor_*", "*.wav")):
             basename = os.path.basename(file)
             emotion = self.int2emotion[basename.split("-")[2]]
-            mfcc, chroma, mel, contrast, tonnetz = self.research_extract_feature(file)
+            mfcc, chroma, mel, contrast, tonnetz = self.extract_feature(file)
             mfccs_arr.append(mfcc)
             chromas_arr.append(chroma)
             mels_arr.append(mel)
@@ -150,8 +84,9 @@ class Voice_Recognizer:
         pickle.dump(contrasts_arr, open("contrast.list", "wb"))    
         pickle.dump(tonnetzs_arr, open("tonnetz.list", "wb"))    
         pickle.dump(y, open("y.list", "wb"))
-    
-    def research_model(self):
+        
+    def full_research(self):
+        print("[*] Loading saved features...")
         mfccs_arr = pickle.load(open("mfcc.list", "rb"))
         chromas_arr = pickle.load(open("chroma.list", "rb"))
         mels_arr = pickle.load(open("mel.list", "rb"))
@@ -164,46 +99,63 @@ class Voice_Recognizer:
             tmp = np.hstack((tmp, mfccs_arr[i]))
             tmp = np.hstack((tmp, chromas_arr[i]))
             tmp = np.hstack((tmp, mels_arr[i]))
-            tmp = np.hstack((tmp, contrasts_arr[i]))
-            tmp = np.hstack((tmp, tonnetzs_arr[i]))
+            # tmp = np.hstack((tmp, contrasts_arr[i]))
+            # tmp = np.hstack((tmp, tonnetzs_arr[i]))
             X.append(tmp)
             del tmp
-        X_train, X_test, y_train, y_test = train_test_split(np.array(X), y, test_size = 0.2)
-        print("[+] Number of training samples:", X_train.shape[0])
-        print("[+] Number of testing samples:", X_test.shape[0])
-        print("[+] Number of features:", X_train.shape[1])
-        from sklearn.preprocessing import StandardScaler
+        print("[*] Preprocessing data...")
+        X = np.array(X)
+        scaler = MinMaxScaler(feature_range=(-1,1))
+        X = scaler.fit_transform(X)
         scaler = StandardScaler()
-        scaler.fit(X_train)
-        X_train = scaler.transform(X_train)
-        X_test = scaler.transform(X_test)
-        model_params = {
-            'alpha': 0.001,
+        scaler.fit(X)
+        X = scaler.transform(X)
+        X_train_l0, X_test_l0, y_train_l0, y_test_l0 = train_test_split(X, y, test_size = 0.2)
+        print("[*] Training MLPClassifier NN with adam solver...")
+        adam_model_params = {
+            'alpha': 0.0001,
+            'batch_size': 300,
+            'epsilon': 0.01,
+            'hidden_layer_sizes': (1000,),
+            'max_iter': 5000,
+            'solver': 'adam',
+            'activation': 'tanh',
+            'shuffle': True,
+            'n_iter_no_change': 50,
+        }
+        adam_model = MLPClassifier(**adam_model_params)
+        adam_model.fit(X_train_l0, y_train_l0)
+        print("[*] Testing MLPClassifier NN with adam solver...")
+        accuracy = accuracy_score(y_true = y_test_l0, y_pred = adam_model.predict(X_test_l0))
+        print("\tAccuracy: {:.2f}%".format(accuracy*100))
+        print("[*] Training MLPClassifier NN with lbfgs solver...")
+        lbgfs_model_params = {
+            'alpha': 0.0001,
             'batch_size': 300,
             'hidden_layer_sizes': (1000,), 
-            'max_iter': 20000,
+            'max_iter': 5000,
             'solver': 'lbfgs',
-            'activation': 'logistic',
-            'verbose': True
+            'activation': 'tanh',
         }
-        model = MLPClassifier(**model_params)
-        print("[*] Training the model...")
-        model.fit(X_train, y_train)
-        pickle.dump(model, open("mlp_classifier.model", "wb"))
-        print("[*] Testing the model...")
-        y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_true = y_test, y_pred = y_pred)
-        print("Accuracy: {:.2f}%".format(accuracy*100))
-        
-class Recorder:
-
-    def __init__(self):
-        self.freq = 48000
-
-    def record(self, duration = 10):
-        print("recording...")
-        recording = sd.rec(int(duration * self.freq), 
-                samplerate = self.freq, channels = 1)
-        sd.wait()
-        print("writing recording to disk...")
-        write("live_recording.wav", self.freq, recording)
+        lbgfs_model = MLPClassifier(**lbgfs_model_params)
+        lbgfs_model.fit(X_train_l0, y_train_l0)
+        print("[*] Testing MLPClassifier NN with lbfgs solver...")
+        accuracy = accuracy_score(y_true = y_test_l0, y_pred = lbgfs_model.predict(X_test_l0))
+        print("\tAccuracy: {:.2f}%".format(accuracy*100))
+        X_train_l1, X_test_l1, y_train_l1, y_test_l1 = train_test_split(np.array(X_test_l0), y_test_l0, test_size = 0.2)
+        adam_preds = adam_model.predict_proba(X_train_l1)
+        lbgfs_preds = lbgfs_model.predict_proba(X_train_l1)
+        stackX = adam_preds
+        stackX = np.dstack((stackX, lbgfs_preds))
+        stackX = stackX.reshape((stackX.shape[0], stackX.shape[1]*stackX.shape[2]))
+        model = LogisticRegression()
+        print("[*] Training LogisticRegression ML ensemble model...")
+        model.fit(stackX, y_train_l1)
+        adam_preds = adam_model.predict_proba(X_test_l1)
+        lbgfs_preds = lbgfs_model.predict_proba(X_test_l1)
+        stackX = adam_preds
+        stackX = np.dstack((stackX, lbgfs_preds))
+        stackX = stackX.reshape((stackX.shape[0], stackX.shape[1]*stackX.shape[2]))
+        print("[*] Testing LogisticRegression ML ensemble model...")
+        accuracy = accuracy_score(y_true = y_test_l1, y_pred = model.predict(stackX))
+        print("\tAccuracy: {:.2f}%".format(accuracy*100))
--- main.py
+++ main.py
@@ -5,28 +5,31 @@ from audio import *
 import numpy as np
 
 def main():
-    ocv_files = 'opencv_files'
+    """ocv_files = 'opencv_files'
     cascade_classifier = 'haarcascade_frontalface_alt.xml'
     subjects = ["", "Amber Heard", "Bill Gates", "Jason Momoa", "Paul Rudd", 
             "Scarlett Johansson"]
     img_len = 400
     img_width = 300
-    # face_rec = Face_Recognizers(os.path.join(ocv_files, cascade_classifier),
-            # img_len, img_width, subjects, os.path.join("data_files",
-                # "face_training"), os.path.join("data_files", "face_test"))
+    face_rec = Face_Recognizers(os.path.join(ocv_files, cascade_classifier),
+            img_len, img_width, subjects, os.path.join("data_files",
+                "face_training"), os.path.join("data_files", "face_test"))
+    face_rec.predict_all()
+    face_rec.video_cap()"""
     voice_rec = Voice_Recognizer(os.path.join("data_files", "voice_training"))
-    # recorder = Recorder()
-    # face_rec.predict_all()
-    # face_rec.video_cap()
+    voice_rec.full_research()
+    # voice_rec.train_adam()
+    # voice_rec.train_lbgfs()
+    # voice_rec.train_ensemble()
     # files = os.listdir("demo")
+    # arr = []
     # for file in files:
-        # print(file, end = ' ')
-        # print(voice_rec.predict(os.path.join("demo", file)))
-    # recorder.record()
-    # voice_rec.predict("live_recording.wav")
-    voice_rec.research_model()
-    # voice_rec.research_load_data()
-
+        # femo = file.split("_")[2].split(".")[0]
+        # if (femo == 'ps'):
+            # continue
+        # if (femo == 'fear'):
+            # femo = 'fearful'
+        # arr.append([femo, voice_rec.predict_ensemble(os.path.join("demo", file))])
     
 if __name__ == "__main__":
     main()

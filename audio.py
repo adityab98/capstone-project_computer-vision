52eb2b506bf3293be7e8138f01100bbe8ba82c56
Aditya Bhargav <adityabhargav9876@gmail.com>
2022-04-17 11:56:56 +0530

    Finished with ensembling, for now

--- audio.py
+++ audio.py
@@ -11,6 +11,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 
 warnings.filterwarnings("ignore")
 
@@ -134,44 +135,45 @@ class Voice_Recognizer:
         for i in range(len(mfccs_arr)):
             tmp = np.array([])
             tmp = np.hstack((tmp, mfccs_arr[i]))
-            tmp = np.hstack((tmp, chromas_arr[i]))
-            tmp = np.hstack((tmp, mels_arr[i]))
+            # tmp = np.hstack((tmp, chromas_arr[i]))
+            # tmp = np.hstack((tmp, mels_arr[i]))
             tmp = np.hstack((tmp, contrasts_arr[i]))
-            tmp = np.hstack((tmp, tonnetzs_arr[i]))
+            # tmp = np.hstack((tmp, tonnetzs_arr[i]))
             X.append(tmp)
             del tmp
         print("[*]L0 Preprocessing data...")
         X = self.normalize(X)
         X_train_l0, X_test_l0, y_train_l0, y_test_l0 = train_test_split(X, y, test_size = 0.4, random_state = 12)
         adam_model = pickle.load(open("adam.model", "rb"))
-        lbgfs_model = pickle.load(open("lbgfs.model", "rb"))
+        lbfgs_model = pickle.load(open("lbfgs.model", "rb"))
         # print("[*]L0 Training MLPClassifier NN with adam solver...")
         # adam_model_params = {'batch_size': 300, 'epsilon': 0.01, 'hidden_layer_sizes': (1000,), 'max_iter': 5000, 'solver': 'adam', 'activation': 'tanh', 'n_iter_no_change': 50,}
         # adam_model = MLPClassifier(**adam_model_params)
         # adam_model.fit(X_train_l0, y_train_l0)
-        print("[*]L0 Testing MLPClassifier NN with adam solver...")
-        accuracy = accuracy_score(y_true = y_test_l0, y_pred = adam_model.predict(X_test_l0))
-        print("\tAccuracy: {:.2f}%".format(accuracy*100))
+        # print("[*]L0 Testing MLPClassifier NN with adam solver...")
+        # accuracy = accuracy_score(y_true = y_test_l0, y_pred = adam_model.predict(X_test_l0))
+        # print("\tAccuracy: {:.2f}%".format(accuracy*100))
         # print("[*]L0 Training MLPClassifier NN with lbfgs solver...")
-        # lbgfs_model_params = {'batch_size': 300, 'hidden_layer_sizes': (1000,),  'max_iter': 5000, 'solver': 'lbfgs', 'activation': 'tanh'}
-        # lbgfs_model = MLPClassifier(**lbgfs_model_params)
-        # lbgfs_model.fit(X_train_l0, y_train_l0)
-        print("[*]L0 Testing MLPClassifier NN with lbfgs solver...")
-        accuracy = accuracy_score(y_true = y_test_l0, y_pred = lbgfs_model.predict(X_test_l0))
-        print("\tAccuracy: {:.2f}%".format(accuracy*100))
+        # lbfgs_model_params = {'batch_size': 300, 'hidden_layer_sizes': (1000,),  'max_iter': 2000, 'solver': 'lbfgs', 'activation': 'tanh'}
+        # lbfgs_model = MLPClassifier(**lbfgs_model_params)
+        # lbfgs_model.fit(X_train_l0, y_train_l0)
+        # print("[*]L0 Testing MLPClassifier NN with lbfgs solver...")
+        # accuracy = accuracy_score(y_true = y_test_l0, y_pred = lbfgs_model.predict(X_test_l0))
+        # print("\tAccuracy: {:.2f}%".format(accuracy*100))
         X_train_l1, X_test_l1, y_train_l1, y_test_l1 = train_test_split(X_test_l0, y_test_l0, test_size = 0.2)
-        stackX = self.get_stacked(X_train_l1, [adam_model, lbgfs_model])
-        model = LogisticRegression()
+        stackX = self.get_stacked(X_train_l1, [adam_model, lbfgs_model])
+        ensemble_model_params = {'criterion': 'entropy', 'splitter': 'best', 'ccp_alpha': 0.15}
+        model = DecisionTreeClassifier(**ensemble_model_params)
         print("[*]L1 Training LogisticRegression ML ensemble model...")
         model.fit(stackX, y_train_l1)
-        stackX = self.get_stacked(X_test_l1, [adam_model, lbgfs_model])
+        stackX = self.get_stacked(X_test_l1, [adam_model, lbfgs_model])
         print("[*]L1 Testing LogisticRegression ML ensemble model...")
         accuracy = accuracy_score(y_true = y_test_l1, y_pred = model.predict(stackX))
         print("\tAccuracy: {:.2f}%".format(accuracy*100))
         print("[*] Saving models to disk...")
+        # pickle.dump(adam_model, open("adam.model", "wb"))
+        # pickle.dump(lbfgs_model, open("lbfgs.model", "wb"))
         pickle.dump(model, open("ensemble.model", "wb"))
-        pickle.dump(adam_model, open("adam.model", "wb"))
-        pickle.dump(lbgfs_model, open("lbgfs.model", "wb"))
         
     def research_adam(self):
         mfccs_arr = pickle.load(open("ravdess_mfcc.list", "rb"))
@@ -192,7 +194,7 @@ class Voice_Recognizer:
             # comment each of these for different feature combinations
             tmp = np.hstack((tmp, mfccs_arr[i]))
             # tmp = np.hstack((tmp, chromas_arr[i]))
-            # tmp = np.hstack((tmp, mels_arr[i]))
+            #tmp = np.hstack((tmp, mels_arr[i]))
             tmp = np.hstack((tmp, contrasts_arr[i]))
             # tmp = np.hstack((tmp, tonnetzs_arr[i]))
             ##########################################################
@@ -227,9 +229,67 @@ class Voice_Recognizer:
         accuracy = accuracy_score(y_true = y_test, y_pred = adam_model.predict(X_test))
         print("\tAccuracy: {:.2f}%".format(accuracy*100))
 
+    def research_lbfgs(self):
+        mfccs_arr = pickle.load(open("ravdess_mfcc.list", "rb"))
+        chromas_arr = pickle.load(open("ravdess_chroma.list", "rb"))
+        mels_arr = pickle.load(open("ravdess_mel.list", "rb"))
+        contrasts_arr = pickle.load(open("ravdess_contrast.list", "rb"))
+        tonnetzs_arr = pickle.load(open("ravdess_tonnetz.list", "rb"))
+        y = pickle.load(open("ravdess_y.list", "rb"))
+        mfccs_arr.extend(pickle.load(open("tess_mfcc.list", "rb")))
+        chromas_arr.extend(pickle.load(open("tess_chroma.list", "rb")))
+        mels_arr.extend(pickle.load(open("tess_mel.list", "rb")))
+        contrasts_arr.extend(pickle.load(open("tess_contrast.list", "rb")))
+        tonnetzs_arr.extend(pickle.load(open("tess_tonnetz.list", "rb")))
+        y.extend(pickle.load(open("tess_y.list", "rb")))
+        X = []
+        for i in range(len(mfccs_arr)):
+            tmp = np.array([])
+            # comment each of these for different feature combinations
+            tmp = np.hstack((tmp, mfccs_arr[i]))
+            # tmp = np.hstack((tmp, chromas_arr[i]))
+            # tmp = np.hstack((tmp, mels_arr[i]))
+            tmp = np.hstack((tmp, contrasts_arr[i]))
+            # tmp = np.hstack((tmp, tonnetzs_arr[i]))
+            ##########################################################
+            X.append(tmp)
+            del tmp
+        print("Without normalization or NN optimization:")
+        X_train, X_test, y_train, y_test = train_test_split(np.array(X), y, test_size = 0.2, random_state = 12)
+        lbfgs_model_params = {'batch_size': 300, 'hidden_layer_sizes': (1000,), 'max_iter': 2000, 'solver': 'lbfgs'}
+        lbfgs_model = MLPClassifier(**lbfgs_model_params)
+        lbfgs_model.fit(X_train, y_train)
+        accuracy = accuracy_score(y_true = y_test, y_pred = lbfgs_model.predict(X_test))
+        print("\tAccuracy: {:.2f}%".format(accuracy*100))
+        print("With normalization but without NN optimization:")
+        X_train, X_test, y_train, y_test = train_test_split(self.normalize(X), y, test_size = 0.2, random_state = 12)
+        lbfgs_model_params = {'batch_size': 300, 'hidden_layer_sizes': (1000,), 'max_iter': 2000, 'solver': 'lbfgs'}
+        lbfgs_model = MLPClassifier(**lbfgs_model_params)
+        lbfgs_model.fit(X_train, y_train)
+        accuracy = accuracy_score(y_true = y_test, y_pred = lbfgs_model.predict(X_test))
+        print("\tAccuracy: {:.2f}%".format(accuracy*100))
+        print("Without normalization but with NN optimization:")
+        X_train, X_test, y_train, y_test = train_test_split(np.array(X), y, test_size = 0.2, random_state = 12)
+        lbfgs_model_params = {'batch_size': 300, 'hidden_layer_sizes': (1000,),  'max_iter': 2000, 'solver': 'lbfgs', 'activation': 'tanh'}
+        lbfgs_model = MLPClassifier(**lbfgs_model_params)
+        lbfgs_model.fit(X_train, y_train)
+        accuracy = accuracy_score(y_true = y_test, y_pred = lbfgs_model.predict(X_test))
+        print("\tAccuracy: {:.2f}%".format(accuracy*100))
+        print("With normalization and NN optimization:")
+        X_train, X_test, y_train, y_test = train_test_split(self.normalize(X), y, test_size = 0.2, random_state = 12)
+        lbfgs_model_params = {'batch_size': 300, 'hidden_layer_sizes': (1000,),  'max_iter': 2000, 'solver': 'lbfgs', 'activation': 'tanh'}
+        lbfgs_model = MLPClassifier(**lbfgs_model_params)
+        lbfgs_model.fit(X_train, y_train)
+        accuracy = accuracy_score(y_true = y_test, y_pred = lbfgs_model.predict(X_test))
+        print("\tAccuracy: {:.2f}%".format(accuracy*100))
+
 def main():
     voice_rec = Voice_Recognizer(os.path.join("data_files", "voice_training"))
-    voice_rec.research_adam()
+    # voice_rec.save_ravdess_features()
+    # voice_rec.save_tess_features()
+    # voice_rec.research_adam()
+    # voice_rec.research_lbfgs()
+    voice_rec.full_research()
     print("***********************************I AM DONE**********************************************")
     
 if __name__ == "__main__":
